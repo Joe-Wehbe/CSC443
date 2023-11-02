ae079f282499b0952cd7fcd4fba37c4bada3d200
Joe-Wehbe <joewehbe@yahoo.com>
2023-11-02 18:00:07 +0200

    Created an html table for the countries

--- Assignment2/q1/q1.php
+++ Assignment2/q1/q1.php
@@ -1,30 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
 <?php
-$host='localhost';
-$dbName='sakila';
-$user='root';
-$password='root';
+$host = 'localhost';
+$dbName = 'sakila';
+$user = 'root';
+$password = 'root';
 
 try {
-	$pdo = new PDO('mysql:host='.$host.';dbname='.$dbName,$user, $password) ;
-    echo 'connection  working';
-}
-catch (PDOException $e) {
-	echo "Error: ".$e->getMessage()."<br/>" 	;
- 	die() ;
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $dbName, $user, $password);
+    echo 'Connection working' . "<br/>" . "<br/>";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage() . "<br/>";
+    die();
 }
 
-$sql="select * from country";
+$sql = "select * from country";
 $stm = $pdo->prepare($sql);
 $stm->execute();
 
 $countries = $stm->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($countries as $country)
-{
-    foreach($country as $fieldName => $fieldValue)
-        echo $fieldName.':'.$fieldValue;
-    echo '<br>';
-}
-
-
 ?>
+<table border="1" style="border-collapse: collapse">
+    <?php foreach ($countries as $country) { ?>
+        <tr>
+            <?php foreach ($country as $fieldName => $fieldValue) { ?>
+                <td>
+                    <?php echo $fieldName . ':' . $fieldValue . " "; ?>
+                </td>
+            <?php } ?>
+        </tr>
+    <?php } ?>
+</table>
+</html>
