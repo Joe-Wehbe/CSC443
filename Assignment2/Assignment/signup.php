df56be238848c189dc18cca70957ee8700b18785
Joe-Wehbe <joewehbe@yahoo.com>
2023-11-02 18:53:09 +0200

    Created a sign up form

--- Assignment2/Assignment/signup.php
+++ Assignment2/Assignment/signup.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
 
 <html>
-
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="style.css?<?php echo time() ?>" />
-        <title></title>
+        <title>Sign up</title>
     </head>
 
-    <body>
-        
-    </body>
+    <?php require_once("..\pdo.php");?>
 
+    <body>
+        <h1>Welcome</h1>
+
+        <form method = "POST" action="signup.php">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" name="sign up" value="Sign up">
+        </form> 
+
+        <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $username = $_POST["username"];
+            $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+    
+            $stm = $pdo->prepare("INSERT INTO users (username, password) VALUES(?,?)");
+            $stm->bindParam(1, $username);
+            $stm->bindParam(2, $password);
+            $stm->execute();
+        }
+        ?>
+
+    </body>
 </html
