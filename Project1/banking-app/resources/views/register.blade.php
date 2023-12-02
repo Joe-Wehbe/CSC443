eb8b176a0d080e8829dea5b7611fbb37c8b53786
Joe-Wehbe <joewehbe@yahoo.com>
2023-12-02 15:50:05 +0200

    Started designing the registration page

---
+++ Project1/banking-app/resources/views/register.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+</head>
+<body>
+    <div class="card">
+        <form action="/register" method="POST">
+            <input type="text" name="fname" placeholder="First name">
+            <input type="text" name="lname" placeholder="Last name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" name="submit" value="Register">
+        </form>
+    </div>
+    
+</body>
+</html>
