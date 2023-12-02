281a3604ecab57ea8f6163e3dd31217a21fa493e
Joe-Wehbe <joewehbe@yahoo.com>
2023-12-02 18:09:49 +0200

    Worked on the design of the registration page

--- Project1/banking-app/resources/views/register.blade.php
+++ Project1/banking-app/resources/views/register.blade.php
@@ -5,17 +5,25 @@
     <title>Register</title>
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
+
 <body>
     <div class="card">
-    <img src="{{ asset('images/banking-illustration.png') }}" alt="Your Image">
+        <div class="left-side-container">
+            <div class="statement">
+                <h1> Welcome to your virtual bank </h1>
+                <h3> A place that makes your financial life easier</h3>
+            </div>  
+            <img src="{{ asset('images/banking-illustration.png') }}" alt="Your Image">
+        </div>
+
         <div class="right-side-container">
-            <h1> Welcome to Bank Manager </h1>
-            <h4> A place that makes you financial life easier</h4>
+            <h2> Create an account </h2>
             <form action="/register" method="POST">
                 <input type="text" name="fname" placeholder="First name">
                 <input type="text" name="lname" placeholder="Last name">
                 <input type="email" name="email" placeholder="Email">
                 <input type="password" name="password" placeholder="Password">
+                <input type="password" name="cpassword" placeholder="Confirm password">
                 <input type="submit" name="submit" value="Register">
             </form>
         <div>
