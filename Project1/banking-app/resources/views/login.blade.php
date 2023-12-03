7b747154f6188b81a68569d9b64122e170fddb2b
Joe-Wehbe <joewehbe@yahoo.com>
2023-12-03 21:17:42 +0200

    Designed the login page

--- Project1/banking-app/resources/views/login.blade.php
+++ Project1/banking-app/resources/views/login.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
@@ -11,26 +11,19 @@
     <div class="card">
         <div class="left-side-container">
             <div class="statement1">
-                <h1> Welcome to your virtual bank </h1>
-                <h3> A place that makes your financial life easier</h3>
+                <h1> Welcome back </h1>
+                <h3> Glad to see you again!</h3>
             </div>  
             <img src="{{ asset('images/banking-illustration.png') }}" alt="Img">
         </div>
 
         <div class="right-side-container">
             <div class="statement2">
-                <h2> Create an account </h2>
-                <h4> Take the first step towards secure and convenient banking <h4>
+                <h2> Log into your account </h2>
+                <h4> Manage your accounts, check your balance, and much more. </h4>
             </div>
             <form action="/register" method="POST">
-                <div class="input-container">
-                    <i class="fa fa-user"></i>
-                    <input type="text" name="fname" placeholder="First name">
-                </div>
-                <div class="input-container">
-                    <i class="fa-solid fa-users"></i>                    
-                <input type="text" name="lname" placeholder="Family name">
-                </div>
+
                 <div class="input-container">
                     <i class="fa-solid fa-envelope"></i>
                     <input type="email" name="email" placeholder="Email">
@@ -39,12 +32,9 @@
                     <i class="fa-solid fa-lock"></i>                    
                     <input type="password" name="password" placeholder="Password">
                 </div>
-                <div class="input-container">
-                    <i class="fa-solid fa-circle-check"></i>                    
-                    <input type="password" name="cpassword" placeholder="Confirm password"><br>
-                </div>
-                <input type="submit" name="submit" value="Register">
-                <a href="/login">Already a member? Login </a>
+
+                <input type="submit" name="submit" value="Login">
+                <a href="/">Don't have an account? Sign up </a>
             </form>
         <div>
     </div>
