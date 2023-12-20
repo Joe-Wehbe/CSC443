9339d6decf32dba55b8c8f096d1f4ec9ecae7d4a
Joe-Wehbe <joewehbe@yahoo.com>
2023-12-20 17:54:52 +0200

    Implemented authentication middleware

--- Project1/banking-app/routes/web.php
+++ Project1/banking-app/routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Account;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AccountController;
+use App\Http\Middleware\AuthMiddleware;
 use App\Http\Controllers\TransactionController;
 
 /*
@@ -21,15 +22,15 @@ use App\Http\Controllers\TransactionController;
 //          **************** User Controller ****************
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/accounts', [UserController::class, 'login']);
-Route::get('/logout', [UserController::class, 'logout']);
-Route::get("/user-accounts/{userId}", [UserController::class, 'getUserAccounts']);
+Route::get('/logout', [UserController::class, 'logout'])->middleware(AuthMiddleware::class);
+Route::get("/user-accounts/{userId}", [UserController::class, 'getUserAccounts'])->middleware(AuthMiddleware::class);
 
 
 //          **************** Account Controller ****************
 Route::post('/create-account', [AccountController::class, 'createAccount']);
 Route::post('/update-account-status', [AccountController::class, 'updateAccountStatus']);
-Route::get("/account-details/{accountId}", [AccountController::class, 'getAccountDetails']);
-Route::get("/user-account-details/{accountId}", [AccountController::class, 'getAccountDetailsAdmin']);
+Route::get("/account-details/{accountId}", [AccountController::class, 'getAccountDetails'])->middleware(AuthMiddleware::class);
+Route::get("/user-account-details/{accountId}", [AccountController::class, 'getAccountDetailsAdmin'])->middleware(AuthMiddleware::class);
 Route::post('/update-account-availability', [AccountController::class, 'updateAccountAvailability']);
 
 
@@ -43,27 +44,27 @@ Route::post('/transfer-from', [TransactionController::class, 'transferFrom']);
 //          **************** Navigation Routes ****************
 Route::get("/register", function () {return view('register');});
 Route::get("/login", function () {return view('login');});
-Route::get("/create-account", function () {return view('create-account');});
+Route::get("/create-account", function () {return view('create-account');})->middleware(AuthMiddleware::class);
 Route::get("/about", function () {return view('about');});
 
 Route::get("/accounts", function () {
     if(auth()->check()){$accounts = auth()->user()->userAccounts()->latest()->get();}
     return view('accounts', ['accounts' => $accounts]);
-});
+})->middleware(AuthMiddleware::class);
 
 Route::get("/pending", function () {
     if(auth()->check()){$accounts = auth()->user()->userAccounts()->latest()->get();}
     return view('pending', ['accounts' => $accounts]);
-});
+})->middleware(AuthMiddleware::class);
 
 Route::get("/users", function () {
     if(auth()->check()){$users = User::all();}
     return view('users', ['users' => $users]);
-});
+})->middleware(AuthMiddleware::class);
 
 Route::get("/user-requests", function () {
     if(auth()->check()){$accounts = Account::all(); $users = User::All();}
     return view('user-requests', ['accounts' => $accounts, 'users' => $users]);
-});
+})->middleware(AuthMiddleware::class);
 
 
