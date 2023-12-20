948e0c55c34e2a3e3495721226dfcad66b5425b3
Joe-Wehbe <joewehbe@yahoo.com>
2023-12-20 04:33:08 +0200

    Linked accounts in account-details

--- Project1/banking-app/public/js/account-details.js
+++ Project1/banking-app/public/js/account-details.js
@@ -37,3 +37,12 @@ function closeFromModal() {
     document.getElementById("from-modal-container").style.display = "none";
 }
 
+var cards = document.getElementsByClassName('card');
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function() {
+        var accountId = this.getAttribute('data-account-id');
+        window.location.href = '/account-details/' + accountId;
+    });
+}
+
+
